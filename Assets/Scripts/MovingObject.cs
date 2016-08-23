a7005406444327ff5e75c81ed885db452056b8a9
Slamakans <slamakanss@gmail.com>
2016-08-23 16:50:09 +0200

    Basic player movement and animations
    Added some player movement, and did some animations using Unity's
    AnimationController system thingy.

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovingObject : MonoBehaviour {
+    [SerializeField]
+    protected float MovementSpeed = 4f;
+    protected Rigidbody2D attachedRigidbody;
+    
+    protected virtual void Start()
+    {
+        attachedRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Move(Vector2 dir)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Player : MovingObject {
+    private Animator animator;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+    }
+
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if(h != 0)
+        {
+            attachedRigidbody.AddForce(Vector2.right * MovementSpeed * h, ForceMode2D.Force);
+            var hsign = Mathf.Sign(h);
+            var scalesign = Mathf.Sign(transform.localScale.x);
+            if(scalesign != hsign)
+            {
+                var newScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+                transform.localScale = newScale;
+            }
+        }
+
+        animator.SetBool("moving", Mathf.Abs(attachedRigidbody.velocity.x) > 0.15f);
+	}
+}
