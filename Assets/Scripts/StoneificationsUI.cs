13bb122c3948ec6673f3ca5c19c6d5551913e466
Slamakans <slamakanss@gmail.com>
2016-08-31 17:40:12 +0200

    Placeholder GUI
    Shows how many stoneifications you have left in the form of TV images.

---
+++ Assets/Scripts/StoneificationsUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneificationsUI : MonoBehaviour {
+    public Transform stoneImage;
+
+    private Player player;
+    private int curStones = 0;
+    private ArrayList images = new ArrayList();
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        curStones = player.GetStoneifications();
+        if(curStones < images.Count)
+        {
+            var stone = images[images.Count - 1] as RectTransform;
+            Destroy(stone.gameObject);
+            images.RemoveAt(images.Count - 1);
+        }
+        else if(curStones > images.Count)
+        {
+            var stone = Instantiate(stoneImage, transform) as RectTransform;
+            stone.anchoredPosition = GetPos();
+            images.Add(stone);
+        }
+    }
+
+    Vector3 GetPos()
+    {
+        return new Vector3(50f + (images.Count * 50f), -50f, 0);
+    }
+}
