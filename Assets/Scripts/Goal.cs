444d9d67f9b4a18103b80e449be52ab19484275a
Slamakans <slamakanss@gmail.com>
2016-08-31 17:37:50 +0200

    Goal Added

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Goal : MonoBehaviour {
+    public Object NextLevel;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+            SceneManager.LoadScene(NextLevel.name);
+    }
+}
