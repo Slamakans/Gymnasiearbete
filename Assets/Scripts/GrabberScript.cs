137f37d40d6fc17ed29d8ea54d0adb73ed220e73
Slamakans <slamakanss@gmail.com>
2016-08-31 17:40:53 +0200

    Changed grabbing behaviour
    You can't grab anything while moving upwards anymore.

--- Assets/Scripts/GrabberScript.cs
+++ Assets/Scripts/GrabberScript.cs
@@ -2,23 +2,58 @@
 using System.Collections;
 
 public class GrabberScript : MonoBehaviour {
+    private Transform player;
+    private bool left = false;
+    private Collider2D waiting;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Ledge")
+        player = transform.parent;
+        var playerScript = player.GetComponent<Player>();
+        if (other.tag == "Ledge")
         {
-            var player = transform.parent;
-            var world = player.parent;
-
-            transform.parent = world;
-            player.parent = transform;
-
-            transform.position = other.transform.position;
-            player.GetComponent<Player>().rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
-
-            player.parent = world;
-            transform.parent = player;
-
-            player.GetComponent<Player>().grabbing = true;
+            if (playerScript.IsGrounded()) return;
+            if (playerScript.rb2d.velocity.y < 0)
+            {
+                Grab(other);
+            }
+            else
+            {
+                waiting = other;
+                StartCoroutine(GrabWhenFalling(other));
+            }
         }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other == waiting)
+            left = true;
+    }
+
+    private IEnumerator GrabWhenFalling(Collider2D other)
+    {
+        yield return new WaitUntil(() => player.GetComponent<Player>().rb2d.velocity.y < 0);
+        if (!left)
+        {
+            Grab(other);
+            waiting = null;
+        }
+    }
+
+    private void Grab(Collider2D other)
+    {
+        var world = player.parent;
+
+        transform.parent = world;
+        player.parent = transform;
+
+        transform.position = new Vector3(other.transform.position.x, other.transform.position.y, transform.position.z);
+        player.GetComponent<Player>().rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+
+        player.parent = world;
+        transform.parent = player;
+
+        player.GetComponent<Player>().grabbing = true;
     }
 }
