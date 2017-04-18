4a6393ac2909c876a3d54987d21222dcab45f29b
Slamakans <slamakanss@gmail.com>
2017-04-18 12:11:18 +0200

    Grid script commented out, not needed apparently. Add joystick input

--- Assets/Scripts/GridSnapScript.cs
+++ Assets/Scripts/GridSnapScript.cs
@@ -8,21 +8,24 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class GridSnapScript : MonoBehaviour
 {
-    public float snapValue = 16;
+    public float snapValue = 4;
     public float depth = 0;
 
     void Update()
     {
-        float snapInverse = 1 / snapValue;
+        /*float snapInverse = 1 / snapValue;
 
         float x, y, z;
 
-        // if snapValue = .5, x = 1.45 -> snapInverse = 2 -> x*2 => 2.90 -> round 2.90 => 3 -> 3/2 => 1.5
+        // if snapValue = 16, x = 1.45 -> snapInverse = 0.0625 -> x*0.0625 => 0.090625 -> round 0.090625 => 0 -> 0/0.0625 => 0
         // so 1.45 to nearest .5 is 1.5
-        x = Mathf.Round(transform.position.x * snapInverse) / snapInverse;
-        y = Mathf.Round(transform.position.y * snapInverse) / snapInverse;
+        x = Mathf.Round(Mathf.Round(transform.position.x * snapInverse) / snapInverse);
+        y = Mathf.Round(Mathf.Round(transform.position.y * snapInverse) / snapInverse);
         z = depth;  // depth from camera
 
-        transform.position = new Vector3(x, y, z);
+        //if (x == 0 && y == 0) return;
+        Debug.Log("x: " + x + ", y: " + y);
+
+        transform.position = new Vector3(x, y, z);*/
     }
 }
