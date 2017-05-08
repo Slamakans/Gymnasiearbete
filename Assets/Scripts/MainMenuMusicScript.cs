790a348db048838cac403181f29495f85f69b572
Slamakans <slamakanss@gmail.com>
2017-05-08 18:07:36 +0200

    New level, new splash, new menu music

---
+++ Assets/Scripts/MainMenuMusicScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MainMenuMusicScript : MonoBehaviour
+{
+    public AudioClip engineStartClip;
+    public AudioClip engineLoopClip;
+
+    private AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.loop = true;
+        StartCoroutine(playEngineSound());
+    }
+
+    IEnumerator playEngineSound()
+    {
+        audio.clip = engineStartClip;
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = engineLoopClip;
+        audio.Play();
+    }
+}
--- Assets/Scripts/SplashScreenScript.cs
+++ Assets/Scripts/SplashScreenScript.cs
@@ -3,22 +3,26 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class SplashScreenScript : MonoBehaviour {
+
+    private SpriteRenderer rend;
+
     void Start ()
     {
+        rend = GetComponent<SpriteRenderer>();
+        rend.color = new Color(0, 0, 0, 0);
         StartCoroutine(DoTheThing());
     }
 
     private IEnumerator DoTheThing ()
     {
         float currentTime = 0f;
-        float timeToMove = 1.5f;
-        SpriteRenderer rend = GetComponent<SpriteRenderer>();
-        yield return new WaitForSecondsRealtime(1.5f);
+        float timeToMove = 50f;
+        yield return new WaitForSecondsRealtime(0.5f);
         yield return new WaitUntil(() =>
         {
             currentTime += Time.deltaTime;
-            rend.color = new Color(1, 1, 1, Mathf.Lerp(rend.color.a, 0, currentTime / timeToMove));
-            return rend.color.a == 0;
+            rend.color = new Color(1, 1, 1, Mathf.Lerp(rend.color.a, 1, currentTime / timeToMove));
+            return rend.color.a > 0.99;
         });
 
         SceneManager.LoadScene("MainMenu");
